a6903a8e13857dfdc7311f615c01cf0f6f5ce6df
sylviasmart <133162904+sylviasmart@users.noreply.github.com>
2024-04-05 16:58:43 +0100

    Delete Loops_operations.py

--- Loops_operations.py
+++
@@ -1,8 +0,0 @@
-squares = ['red', 'yellow', 'green', 'purple', 'blue']
-print(squares)
-# for i in range(5):
-    # squares[i] = 'white'
-# print(squares)
-for a in range(3):
-    squares[a] = 'Blue'
-print(squares)
