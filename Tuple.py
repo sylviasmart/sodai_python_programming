3cc6dd17a71961b59cff39d6dc9b0cd031e9586f
sylviasmart <133162904+sylviasmart@users.noreply.github.com>
2024-04-05 16:59:31 +0100

    Delete Tuple.py

--- Tuple.py
+++
@@ -1,7 +0,0 @@
-names = ('Ada', 'Obi', 'Kemi', 'Temi')
-print('Length of names', len(names))
-
-my_list = ('Ada', 'Obi', 'Kemi', 'Temi')
-new_list = ('2024', 'Sylvia')
-new_list = my_list + ('2024', 'Sylvia')
-print('Length of new_list', len(new_list))
