d0f776724dc122b7ee5fcd6f75782fbf8d810683
sylviasmart <133162904+sylviasmart@users.noreply.github.com>
2024-05-30 01:45:12 +0800

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,22 @@
+# Loop is a function that is used to run operations over and over or rather continously until the given instruction or order is met. 
+
+# FOR LOOP: Is used for sequential traversal i.e it is used for iterating over an iterable like string, tuple, list, etc. It falls under the
+# category of 'definite iteration'. Definite iteration mean the number of repetition is specified explicitly in advance. 
+
+# Illustration one
+
+squares = ['red', 'yellow', 'green', 'purple', 'blue']
+print(squares)
+for i in range(5):
+    squares[i] = 'white'
+print(squares)
+for a in range(3):
+    squares[a] = 'Blue'
+print(squares)
+for b in range(5):
+    squares[b] = 'Velvet'
+print(squares)
+print()
+
+# Illustration two
+
